f1ff8d7e42a12eb3f412f7cae4452ed96a759ec0
Mepaprog <147797715+Mepaprog@users.noreply.github.com>
2024-03-19 00:15:42 -0700

    Add files via upload

---
+++ WesitebPrj6/database.php
@@ -0,0 +1,19 @@
+<?php
+
+  $db_server = "localhost";
+  $db_user = "root";
+  $db_pass = "";
+  $db_name = "test";
+  $conn = "";
+  
+  try{
+    $conn = mysqli_connect(
+    $db_server,
+    $db_user,
+    $db_pass,
+    $db_name);
+  }
+  catch(mysqli_sql_exception){
+    echo "<script type='text/javascript'>alert('could not connect');</script>";
+  }
+?>
